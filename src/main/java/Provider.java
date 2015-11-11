e213444a4b649523491cfc4fbd7f9d08dd5282a7
Krystian <kszewczyk@inf.ug.edu.pl>
2015-11-11 16:15:44 +0100

    Dodano Provider.java

---
+++ src/main/java/Provider.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Krystian Szewczyk on 2015-11-11.
+ */
+public class Provider {
+    private int idProvider;
+    private String providerName;
+
+    public Provider (){
+    }
+
+    public Provider (int idProvider, String providerName){
+        super();
+
+        this.idProvider = idProvider;
+        this.providerName = providerName;
+    }
+
+    public int getIdProvider() { return idProvider; }
+    public void setIdProvider(int idProvider) {
+        this.idProvider = idProvider;
+    }
+
+    public String getProviderName() { return providerName; }
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+}
